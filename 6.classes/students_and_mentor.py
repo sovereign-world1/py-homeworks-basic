2a1fee6a0d660801d183d53437d2e600e110bf31
Shakirov Ilnur <112205725+sovereign-world1@users.noreply.github.com>
2025-02-06 20:48:31 +0300

    Update students_and_mentor.py

--- 6.classes/students_and_mentor.py
+++ 6.classes/students_and_mentor.py
@@ -21,7 +21,12 @@ class Mentor:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
- 
+
+class Lecturer(Mentor):
+
+class Reviewer(Mentor):
+    
+
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
  
